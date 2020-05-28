69a74e81d683d4378927ac34156373b61959813c
gjgd <gjgd@protonmail.com>
2020-05-28 18:34:22 +0200

    Add hasExhaustiveContext that checks if all properties are listed in the context

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import jsonld from 'jsonld';
+
+export const hasExhaustiveContext = async (jsonldDocument: any) => {
+  // Remove all keys not present in the jsonld context
+  const expanded = await jsonld.expand(jsonldDocument);
+  const compacted = await jsonld.compact(expanded, jsonldDocument['@context']);
+  // Check which keys have been removed
+  const keys = Object.keys(jsonldDocument);
+  const newKeysSet = new Set(Object.keys(compacted));
+  const difference = keys.filter(key => !newKeysSet.has(key));
+  return difference.length === 0;
 };
