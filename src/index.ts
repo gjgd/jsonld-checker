9d1407fa269dd5b2f1824e0bfef41fe843216014
gjgd <gjgd@protonmail.com>
2020-05-28 22:23:47 +0200

    Add getAllJsonFromString function to extract all JSON objects from a string

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,50 @@ export const hasExhaustiveContext = async (jsonldDocument: any) => {
   const difference = keys.filter(key => !newKeysSet.has(key));
   return difference.length === 0;
 };
+
+const getAllOpenBrackets = (text: string) => {
+  const results: number[] = [];
+  for (let cursor = 0; cursor < text.length; cursor++) {
+    if (text[cursor] === '{') {
+      results.push(cursor);
+    }
+  }
+  return results;
+};
+
+const getStringUntilMatchingBracket = (
+  text: string,
+  openBracketIndex: number
+) => {
+  let substring = text[openBracketIndex];
+  if (substring !== '{') {
+    throw new Error('start must be the index of an opening bracket');
+  }
+  let bracketCount = 1;
+  let cursor = openBracketIndex + 1;
+  const length = text.length;
+  while (bracketCount !== 0 && cursor < length) {
+    const currentChar = text[cursor];
+    if (currentChar === '{') {
+      bracketCount += 1;
+    } else if (currentChar === '}') {
+      bracketCount -= 1;
+    }
+    cursor += 1;
+    substring += currentChar;
+  }
+  try {
+    return JSON.parse(substring);
+  } catch (e) {
+    return null;
+  }
+};
+
+export const getAllJsonFromString = async (text: string) => {
+  const openBrackets = getAllOpenBrackets(text);
+  return openBrackets
+    .map(openBracketIndex =>
+      getStringUntilMatchingBracket(text, openBracketIndex)
+    )
+    .filter(Boolean);
+};
