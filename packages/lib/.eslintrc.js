fd461583f552d41a140985c1bc936493604b2ffa
gjgd <gjgd@protonmail.com>
2020-06-13 20:56:49 +0200

    Add .eslintrc.js file

---
+++ packages/lib/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: `./tsconfig.json`,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["airbnb-typescript"],
+};
