27122e7611e4273a7fcf72c9006543b10e818a9e
gjgd <gjgd@protonmail.com>
2020-06-13 21:26:57 +0200

    Add .eslintrc.js file

---
+++ packages/app/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    // https://github.com/typescript-eslint/typescript-eslint/issues/251
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'prettier/@typescript-eslint',
+    // Has to be the last one in the extends array
+    'plugin:prettier/recommended',
+  ],
+};
