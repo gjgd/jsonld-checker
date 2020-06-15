5c93faf900f852b945fa02fe37429816023f11e4
gjgd <7913565+gjgd@users.noreply.github.com>
2020-06-15 08:16:00 +0000

    Deploying to gh-pages from  @ 78e6b52c1c72b8ee98ff04399a7b5b7612c0d789 🚀

---
+++ precache-manifest.c2f9ddde4fa2d215b0a33ba1f2752b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a369eae27e0932ca499506ae2650d17d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb1b7999e83813065617",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee642923bb9505a50d16",
+    "url": "/static/js/2.3c62b66a.chunk.js"
+  },
+  {
+    "revision": "c1121fe7b7e3f05d27a76efd7cfceda4",
+    "url": "/static/js/2.3c62b66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1b7999e83813065617",
+    "url": "/static/js/main.c4a07a28.chunk.js"
+  },
+  {
+    "revision": "e8f8d07e5f1d314a58ef",
+    "url": "/static/js/runtime-main.ea03f7e9.js"
+  }
+]);
