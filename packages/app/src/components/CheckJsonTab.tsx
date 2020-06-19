b0ddb6aff7d4d5cecbf12e3bd25f45af739cf365
gjgd <gjgd@protonmail.com>
2020-06-20 01:21:30 +0200

    feat: update CSS in json tab

--- packages/app/src/components/CheckJsonTab.tsx
+++ packages/app/src/components/CheckJsonTab.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import JsonEditor from './JsonEditor';
 import JsonLdPlaygroundButton from './JsonLdPlaygroundButton';
 import CheckJsonButton from './CheckJsonButton';
@@ -8,7 +9,22 @@ import JsonLdCheckResult from '../models/JsonLDCheckResult';
 
 const defaultValue = JSON.stringify(defaultValueJson, null, 2);
 
+const useStyles = makeStyles((theme) => ({
+  buttonWrapper: {
+    display: 'flex',
+    margin: theme.spacing(1),
+  },
+  contentWrapper: {
+    display: 'table',
+    margin: theme.spacing(1),
+  },
+  checkResult: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
 const CheckJsonTab: React.FunctionComponent<{}> = () => {
+  const classes = useStyles();
   const [jsonValue, setJsonValue] = React.useState(defaultValue);
   const [result, setResult] = React.useState<JsonLdCheckResult>();
 
@@ -19,10 +35,14 @@ const CheckJsonTab: React.FunctionComponent<{}> = () => {
 
   return (
     <>
-      <CheckJsonButton value={jsonValue} setResult={setResult} />
-      <JsonLdPlaygroundButton value={jsonValue} />
-      <CheckResult result={result} />
-      <JsonEditor onChange={onChange} value={jsonValue} />
+      <div className={classes.buttonWrapper}>
+        <CheckJsonButton value={jsonValue} setResult={setResult} />
+        <JsonLdPlaygroundButton value={jsonValue} />
+      </div>
+      <div className={classes.contentWrapper}>
+        <CheckResult className={classes.checkResult} result={result} />
+        <JsonEditor onChange={onChange} value={jsonValue} />
+      </div>
     </>
   );
 };
--- packages/app/src/components/JsonLdPlaygroundButton.tsx
+++ packages/app/src/components/JsonLdPlaygroundButton.tsx
@@ -3,8 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(2),
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
   },
 }));
 
@@ -14,9 +15,9 @@ const JsonLdPlaygroundLink: React.FunctionComponent<{ value: string }> = ({
   const classes = useStyles();
   const encodedValue = encodeURIComponent(value);
   return (
-    <>
+    <div className={classes.wrapper}>
       <Button
-        className={classes.button}
+        color="primary"
         variant="contained"
         href={`https://json-ld.org/playground/#json-ld=${encodedValue}`}
         target="_blank"
@@ -24,7 +25,7 @@ const JsonLdPlaygroundLink: React.FunctionComponent<{ value: string }> = ({
       >
         Open in JSON-LD Playground
       </Button>
-    </>
+    </div>
   );
 };
 
