4fe7e7203fc6b37ea9ab0ff5a5829cea4b735fc6
gjgd <gjgd@protonmail.com>
2020-06-20 01:40:07 +0200

    feat: log current git commit

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+console.log(`Current commit is ${process.env.REACT_APP_CURRENT_COMMIT}`);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
