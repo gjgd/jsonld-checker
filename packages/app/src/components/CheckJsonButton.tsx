9b2ced22d08b40aa418e2d85ca7eaeede936fecd
gjgd <gjgd@protonmail.com>
2020-06-19 23:04:31 +0200

    feat: add CheckJsonButton component that has a loader

---
+++ packages/app/src/components/CheckJsonButton.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { check } from 'jsonld-checker';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green, red } from '@material-ui/core/colors';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import JsonLdCheckResult from '../models/JsonLDCheckResult';
+import CheckResult from './CheckResult';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    wrapper: {
+      margin: theme.spacing(1),
+      position: 'relative',
+    },
+    buttonSuccess: {
+      backgroundColor: green[500],
+      '&:hover': {
+        backgroundColor: green[700],
+      },
+    },
+    buttonError: {
+      backgroundColor: red[500],
+      '&:hover': {
+        backgroundColor: red[700],
+      },
+    },
+    buttonProgress: {
+      color: green[500],
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginTop: -12,
+      marginLeft: -12,
+    },
+  })
+);
+
+const CheckJsonButton: React.FunctionComponent<{ value: Object }> = ({
+  value,
+}) => {
+  const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [result, setResult] = React.useState<JsonLdCheckResult>();
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+    [classes.buttonError]: error,
+  });
+
+  React.useEffect(() => {
+    setSuccess(false);
+    setError(false);
+  }, [value]);
+
+  const onClick = async () => {
+    if (!loading) {
+      setLoading(true);
+      const res = await check(value);
+      setSuccess(res.ok);
+      setError(!res.ok);
+      setResult(res);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      {success}
+      {error}
+      <div className={classes.wrapper}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={buttonClassname}
+          disabled={loading}
+          onClick={onClick}
+        >
+          Check
+        </Button>
+        {loading && (
+          <CircularProgress size={24} className={classes.buttonProgress} />
+        )}
+        <CheckResult result={result} />
+      </div>
+    </div>
+  );
+};
+
+export default CheckJsonButton;
--- packages/app/src/components/CheckJsonTab.tsx
+++ packages/app/src/components/CheckJsonTab.tsx
@@ -1,34 +1,22 @@
-import Button from '@material-ui/core/Button';
-import { check } from 'jsonld-checker';
 import React from 'react';
-import JsonLdCheckResult from '../models/JsonLDCheckResult';
-import CheckResult from './CheckResult';
 import JsonEditor from './JsonEditor';
 import JsonLdPlaygroundButton from './JsonLdPlaygroundButton';
+import CheckJsonButton from './CheckJsonButton';
+import defaultValueJson from '../data/defaultValue.json';
 
 const defaultValue = JSON.stringify(defaultValueJson, null, 2);
 
 const CheckJsonTab: React.FunctionComponent<{}> = () => {
   const [jsonValue, setJsonValue] = React.useState(defaultValue);
-  const [result, setResult] = React.useState<JsonLdCheckResult>();
 
   const onChange = (value: string) => {
-    setResult(undefined);
     setJsonValue(value);
-  };
-
-  const onClickCheck = async () => {
-    const res = await check(jsonValue);
-    setResult(res);
   };
 
   return (
     <>
-      <Button variant="contained" onClick={onClickCheck}>
-        Check
-      </Button>
+      <CheckJsonButton value={jsonValue} />
       <JsonLdPlaygroundButton value={jsonValue} />
-      <CheckResult result={result} />
       <JsonEditor onChange={onChange} value={jsonValue} />
     </>
   );
