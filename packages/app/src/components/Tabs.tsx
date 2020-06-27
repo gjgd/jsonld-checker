e3b6a772c28b5c0a439644d317c5a966a90bb34d
gjgd <gjgd@protonmail.com>
2020-06-27 17:49:09 +0200

    feat: add two way binding with tab query param

--- packages/app/src/components/Tabs.tsx
+++ packages/app/src/components/Tabs.tsx
@@ -55,32 +55,35 @@ export default function SimpleTabs() {
     defaultTab = 0;
   }
   const classes = useStyles();
-  const [value, setValue] = React.useState(defaultTab);
+  const [tab, setTab] = React.useState(defaultTab);
 
-  const handleChange = (event: ChangeEvent<{}>, newValue: number) => {
-    if (newValue === 2) {
+  const handleChange = (event: ChangeEvent<{}>, newTab: number) => {
+    const newUrl = new URLSearchParams(window.location.search);
+    newUrl.set('tab', `${newTab}`);
+    window.history.pushState(null, '', `/?${newUrl.toString()}`);
+    if (newTab === 2) {
       alert('Not implemented yet');
     } else {
-      setValue(newValue);
+      setTab(newTab);
     }
   };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} centered>
+        <Tabs value={tab} onChange={handleChange} centered>
           <Tab label="Check a single JSON" {...a11yProps(0)} />
           <Tab label="Check a file" {...a11yProps(1)} />
           <Tab label="Check a Github repo" {...a11yProps(2)} />
         </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
+      <TabPanel value={tab} index={0}>
         <CheckJsonTab />
       </TabPanel>
-      <TabPanel value={value} index={1}>
+      <TabPanel value={tab} index={1}>
         <CheckFileTab />
       </TabPanel>
-      <TabPanel value={value} index={2}>
+      <TabPanel value={tab} index={2}>
         <div>TODO</div>
       </TabPanel>
     </div>
