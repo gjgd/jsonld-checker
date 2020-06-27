eb8845aa02843e134097bf06a2dba7f27baaf8c9
gjgd <gjgd@protonmail.com>
2020-06-27 19:45:52 +0200

    feat: add loader to share button

--- packages/app/src/components/ShareButton.tsx
+++ packages/app/src/components/ShareButton.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green } from '@material-ui/core/colors';
 
-const useStyles = makeStyles((theme) => ({
-  wrapper: {
-    margin: theme.spacing(1),
-    position: 'relative',
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    wrapper: {
+      margin: theme.spacing(1),
+      position: 'relative',
+    },
+    // TODO duplicated code with CheckJsonButton progress
+    shareProgress: {
+      color: green[500],
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginTop: -12,
+      marginLeft: -12,
+    },
+  })
+);
 
 const ShareButton: React.FunctionComponent = () => {
   const classes = useStyles();
   const [tinyUrl, setTinyUrl] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
   return (
     <div className={classes.wrapper}>
       <Button
         color="primary"
         variant="contained"
+        disabled={loading}
         onClick={() => {
           const url = `${window.location.toString()}`;
           const tinyUrlService = 'https://tiny.gjgd.fr/';
+          setLoading(true);
           fetch(tinyUrlService, {
             method: 'POST',
             headers: {
@@ -35,19 +51,30 @@ const ShareButton: React.FunctionComponent = () => {
             })
             .then((data) => {
               setTinyUrl(data.shortUrl || '');
+            })
+            .finally(() => {
+              setLoading(false);
             });
         }}
       >
         Share
       </Button>
-      <a
-        className={classes.wrapper}
-        href={`https://${tinyUrl}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {tinyUrl}
-      </a>
+      {loading && (
+        <CircularProgress size={24} className={classes.shareProgress} />
+      )}
+      {tinyUrl && (
+        <span className={classes.wrapper}>
+          <span>Copy the URL or use this short one:</span>
+          <a
+            className={classes.wrapper}
+            href={`https://${tinyUrl}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {tinyUrl}
+          </a>
+        </span>
+      )}
     </div>
   );
 };
