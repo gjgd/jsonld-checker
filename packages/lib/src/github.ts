ac0095834315888ab4a9a4d6a14108ca1e36a7c7
gjgd <gjgd@protonmail.com>
2020-06-28 19:13:07 +0200

    feat: add getFiles functions to list files in a github repo

---
+++ packages/lib/src/github.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+const fetch = require('node-fetch');
+
+const githubApi = async endpoint => {
+  return fetch(`https://api.github.com/repos/${endpoint}`).then(res =>
+    res.json()
+  );
+};
+
+const getFiles = async (user, repo, ref = 'HEAD') => {
+  const all = await githubApi(`${user}/${repo}/git/trees/${ref}?recursive=1`);
+  const files = all.tree.filter(node => node.type === 'blob');
+  return files;
+};
+
+export { getFiles };
