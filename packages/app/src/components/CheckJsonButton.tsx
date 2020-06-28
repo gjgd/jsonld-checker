6af588608332a91065ae54cad22bac406ea25c20
gjgd <gjgd@protonmail.com>
2020-06-28 15:05:26 +0200

    refactor: use LoaderButton in CheckJsonButton

--- packages/app/src/components/CheckJsonButton.tsx
+++ packages/app/src/components/CheckJsonButton.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import { check } from 'jsonld-checker';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { green, red } from '@material-ui/core/colors';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import { getQueryParameter } from '../utils';
+import LoaderButton from './LoaderButton';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -29,14 +28,6 @@ const useStyles = makeStyles((theme: Theme) =>
         backgroundColor: red[700],
       },
     },
-    buttonProgress: {
-      color: green[500],
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      marginTop: -12,
-      marginLeft: -12,
-    },
   })
 );
 
@@ -46,7 +37,6 @@ const CheckJsonButton: React.FunctionComponent<{
 }> = ({ value, setResult }) => {
   const analyzeQueryParameter = getQueryParameter('analyze');
   const classes = useStyles();
-  const [loading, setLoading] = React.useState(false);
   const [success, setSuccess] = React.useState(false);
   const [error, setError] = React.useState(false);
 
@@ -56,15 +46,11 @@ const CheckJsonButton: React.FunctionComponent<{
   });
 
   const analyzeObject = async () => {
-    if (!loading) {
-      setLoading(true);
-      const res = await check(value);
-      setSuccess(res.ok);
-      setError(!res.ok);
-      setResult(res);
-      setLoading(false);
-      // updateQueryParameter('analyze', '1');
-    }
+    const res = await check(value);
+    setSuccess(res.ok);
+    setError(!res.ok);
+    setResult(res);
+    // updateQueryParameter('analyze', '1');
   };
 
   React.useEffect(() => {
@@ -84,18 +70,11 @@ const CheckJsonButton: React.FunctionComponent<{
       {success}
       {error}
       <div className={classes.wrapper}>
-        <Button
-          variant="contained"
-          color="primary"
+        <LoaderButton
           className={buttonClassname}
-          disabled={loading}
           onClick={analyzeObject}
-        >
-          Check
-        </Button>
-        {loading && (
-          <CircularProgress size={24} className={classes.buttonProgress} />
-        )}
+          buttonText="Check"
+        />
       </div>
     </div>
   );
