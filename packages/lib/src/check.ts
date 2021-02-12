676f8d09c818500aa34abe779454793209355f86
gjgd <gjgd@protonmail.com>
2021-02-12 16:47:04 -0600

    chore: add comments and rename variables to make the code more understandable

--- packages/lib/src/check.ts
+++ packages/lib/src/check.ts
@@ -21,22 +21,34 @@ const check = async (
     } else {
       jsonldDoc = jsonldDocument;
     }
-    // Remove all keys not present in the jsonld context
+    // With this transformation we obtain a modified jsonld doc
+    // where all properties not present in the context are dropped
     const expanded = await jsonld.expand(jsonldDoc, { documentLoader });
-    const compacted = await jsonld.compact(expanded, jsonldDoc['@context'], {
-      documentLoader,
-    });
-    // Check which keys have been removed
-    const keys = Object.keys(flatten(jsonldDoc, { delimiter }))
+    const jsonLdDocWithDroppedProperties = await jsonld.compact(
+      expanded,
+      jsonldDoc['@context'],
+      {
+        documentLoader,
+      }
+    );
+    // Here we get a list of all properties defined in the context
+    // and apply a few transformations to them to clean them up
+    const propertiesDefinedInContext = Object.keys(
+      flatten(jsonLdDocWithDroppedProperties, { delimiter })
+    ).map(removeArrayArtifacts);
+    // Here we get a list of all the properties in the jsonld doc
+    // and apply a few transformations to them to clean them up
+    const allProperties = Object.keys(flatten(jsonldDoc, { delimiter }))
       .filter(isNotJsonLdPropery)
       .map(trimTrailingId)
       .map(removeArrayArtifacts)
       .filter(onlyKeepUnique);
-
-    const compactedKeys = Object.keys(flatten(compacted, { delimiter }))
-      .map(removeArrayArtifacts)
-      .filter(onlyKeepUnique);
-    const difference = keys.filter(key => !compactedKeys.includes(key));
+    // Here we compute the difference between the two lists to obtain
+    // properties in the jsonld doc that are not defined in the context
+    const propertiesDefinedInContextSet = new Set(propertiesDefinedInContext);
+    const difference = allProperties.filter(
+      property => !propertiesDefinedInContextSet.has(property)
+    );
     if (difference.length === 0) {
       return new CheckResult(true);
     }
